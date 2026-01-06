2201b640f31b9a8bcf31a6dd6108340f2f04c149
Mitalicops <120451953+Mitalicops@users.noreply.github.com>
2026-01-06 13:55:58 +0500

    03: Database setup

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,15 +2,15 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/api/inngest(.*)"]);
 
-// This is a middleware that will protect all routes except public routes and below 
+// This is a middleware that will protect all routes except public routes and below
 // the code will be executed if the route is not public it will redirect to the sign in page
-// you can use tradtional warning to user if they arent 
+// you can use tradtional warning to user if they arent
 // logged in by giving em a button to go tot sign in page or u can use this below approach of reidrecting
 
 //export default clerkMiddleware(async (auth, req) => {
-  //if (!isPublicRoute(req)) {
-    //await auth.protect();
-  //}
+//if (!isPublicRoute(req)) {
+//await auth.protect();
+//}
 //});
 
 export default clerkMiddleware();
